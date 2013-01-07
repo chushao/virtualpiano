43666793a93adbfeaf29f6e4f7ce47f0422823f8
Chu Shao <cshao@churenshao.com>
2013-01-07 00:48:24 -0800

    Cleaned up to only show source java file.

--- README.md
+++
@@ -1,5 +0,0 @@
-virtualpiano
-============
-
-Simple Piano Application used to generate tones. 
-Made for MUSIC 170, except was never used in any actual application. 
--- gen/com/cshao/mus170/virtual/piano/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.cshao.mus170.virtual.piano;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
